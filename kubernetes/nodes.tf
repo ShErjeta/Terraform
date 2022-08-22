4e62cc6051dce7503f3b0f926366dc47fc0426a6
ShErjeta <110969709+ShErjeta@users.noreply.github.com>
2022-08-22 23:10:00 +0200

    Update nodes.tf

--- kubernetes/nodes.tf
+++ kubernetes/nodes.tf
@@ -7,13 +7,13 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["099720109477"] # Canonical
+  owners = ["133241726574"] # Canonical
 }
 
 resource "aws_instance" "bastion" {
