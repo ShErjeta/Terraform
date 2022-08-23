1c0600f502c8ac8899d2982195e82eea6a10b41e
ShErjeta <110969709+ShErjeta@users.noreply.github.com>
2022-08-23 15:13:41 +0200

    Update nodes.tf

--- kubernetes/nodes.tf
+++ kubernetes/nodes.tf
@@ -1,18 +1,19 @@
 resource "aws_key_pair" "sshkey" {
-  public_key = var.ssh_public_key
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCb/qtCvn8OM384/wAx8/j//fApSJ6RMz8yyAgzmywRZBCTyDRnQQsZ+Wq87CJsBNe15oL2uMFFBKB6suaSa8LpT4coSdb7w28LMbzOFjWQRHwQSGBtdlgyK82h7eIexBVed2pFOSMpVF/nl0RR/u1BHpLtmoEjHaIFz0N7oS7fhTrkysZeoFBRxH2zFq4eJ9tH9Ig7EwdCvdpWlAdGUHk4St7l2MjG8GqPEqwA3Mwf/SBnW9lOGD7J12HTX+gx4jGFdV2NoqBpXLu9dy2lrpwCHrgY2LRCDrhMSmJFglYrpRIM+gGt2472ar3rXYozCtNeuTtrX29Sx2fD9Y6otTBwm1K5Ke1cuNk2jlwV27ywYO0r9Uk8QWtnPzhUAb0ae5xe3ZKcjlUXtgYUgoxAvkPCg6PuYxxqOgY52lkIwqqmqWnjeHAWjH0aUOEYlxSwyHlXQtp2EkXG5BcppQPY152MN7EOTgfILgp+gKcgJzLT1pUxukgW2x77dGsDkOEgZVs= root@ip-172-31-45-235"
+  
   key_name   = "sshkey"
 }
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["133241726574"] # Canonical
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "bastion" {
